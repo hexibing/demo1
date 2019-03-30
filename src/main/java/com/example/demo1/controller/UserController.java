8de07a4134b4ecdb5ee00207e75c3937a2a256b8
hexibing <1057420677@qq.com>
2019-03-30 16:42:43 +0800

    导入log4j输入日志到控制台

--- src/main/java/com/example/demo1/controller/UserController.java
+++ src/main/java/com/example/demo1/controller/UserController.java
@@ -2,13 +2,18 @@ package com.example.demo1.controller;
 
 import com.example.demo1.model.User;
 import com.example.demo1.service.IUserService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+@RequestMapping("/user")
+public class UserController {
+
+    private  static Logger logger=Logger.getLogger(UserController.class);
+
 
     @Autowired
     private IUserService userService;
@@ -21,6 +26,8 @@ public class HelloController {
 
     @RequestMapping(value = "/getInfo")
     public User getInfo(){
+
+        logger.info("aaaa");
         return userService.getUserInfo("1");
     }
 
